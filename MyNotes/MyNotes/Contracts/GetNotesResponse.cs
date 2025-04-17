2d2a768a86823a52b89181a448ce8f084b70547e
jasmine-green-tea <shelldary@yandex.ru>
2025-04-17 19:56:52 +0300

    backend part

---
+++ MyNotes/MyNotes/Contracts/GetNotesResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace MyNotes.Contracts
+{
+    public record GetNotesResponse(List<NoteDto> notes);
+
+    public record NoteDto(Guid Id, string Title, string Description, DateTime CreatedAt);
+}
